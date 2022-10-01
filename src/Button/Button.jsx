7a5f8bafb12011e3161a0a244d5ffb6cf8318366
Olga Smirnova <g.o.n.z.o.m.e@yandex.ru>
2022-10-01 23:56:35 +0300

    компонент Button помещен в Header

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import classNames from 'classnames';
 
-export function Button({outline}) {
+export function Button({outline, className, children}) {
     return (
-    <button className={ classNames('button', {
+    <button className={ classNames('button', className, {
         'button-outline': outline,
-    })
-    }/>
+    })}>
+        {children}
+    </button>
     )
 }
