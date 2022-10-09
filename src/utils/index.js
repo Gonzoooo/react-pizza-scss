8130c452630fa3c3efb768cb47c85b9eae3361aa
Olga Smirnova <g.o.n.z.o.m.e@yandex.ru>
2022-10-09 23:43:58 +0300

    настроена работа popup-sort

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
 export { default as Button } from '../components/Button/Button';
 export { default as Categories } from '../components/Categories/Categories';
 export { default as Header } from '../components/Header/Header';
+export { default as SortPopup } from '../components/SortPopup/SortPopup';
