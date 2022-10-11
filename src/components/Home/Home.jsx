73b47d7a9f9260be8e223ffbd42e5cf2ca74a6e3
Olga Smirnova <g.o.n.z.o.m.e@yandex.ru>
2022-10-12 02:04:48 +0300

    добавлена имитация сервера, PropTypes для проверки типа данных, axios

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,25 @@
+import {Categories, SortPopup} from "../../utils/index";
+import PizzaBlock from "../PizzaBlock/PizzaBlock";
+
+function Home({items}) {
+    return (
+       <>
+           <div className="container">
+               <div className="content__top">
+                   <Categories items={['Мясные','Вегетарианская','Гриль','Острые','Закрытые']} onClick={(item)=> alert(item)}/>
+                   <SortPopup items={['популярности','цене','алфавиту']}/>
+               </div>
+               <h2 className="content__title">Все пиццы</h2>
+               <div className="content__items">
+                   {
+                       items.map((item)=>(
+                           <PizzaBlock key={item.id} {...item}/>
+                       ))
+                   }
+               </div>
+           </div>
+       </>
+    )
+}
+
+export default Home;
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,5 @@ export { default as Button } from '../components/Button/Button';
 export { default as Categories } from '../components/Categories/Categories';
 export { default as Header } from '../components/Header/Header';
 export { default as SortPopup } from '../components/SortPopup/SortPopup';
+export { default as Home } from '../components/Home/Home';
+export { default as Cart } from '../components/Cart/Cart';
